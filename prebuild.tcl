401aa1e9c4aa9978653f2b35ebd1b190b8331f47
gorogoro123 <gorogoro123@gmail.com>
2019-02-25 22:02:38 +0900

    fix octet value.

--- prebuild.tcl
+++ prebuild.tcl
@@ -1,8 +1,14 @@
-proc make_header {} {
-	set cmd {date +%Y/%m/%d/%H/%M/%S}
-	puts $cmd
+#https://stackoverflow.com/questions/2110864/handling-numbers-with-leading-zeros-in-tcl
+proc forceInteger { x } {
+    set count [scan $x %d%s n rest]
+    if { $count <= 0 || ( $count == 2 && ![string is space $rest] ) } {
+        return -code error "not an integer: \"$x\""
+    }
+    return $n
+}
 
-	set ret [eval exec $cmd]
+proc make_header {} {
+	set ret [clock format [clock seconds] -format {%Y/%m/%d/%H/%M/%S}]
 	puts $ret
 
 	set dst [split $ret '/']
@@ -11,12 +17,12 @@ proc make_header {} {
 	puts $fd [format "#ifndef _config_h_"]
 	puts $fd [format "#define _config_h_"]
 	puts $fd [format ""]
-	puts $fd [format "#define DATE_YEAR  (%d)" [lindex $dst 0]]
-	puts $fd [format "#define DATE_MONTH (%d)" [lindex $dst 1]]
-	puts $fd [format "#define DATE_DAY   (%d)" [lindex $dst 2]]
-	puts $fd [format "#define DATE_HOUR  (%d)" [lindex $dst 3]]
-	puts $fd [format "#define DATE_MIN   (%d)" [lindex $dst 4]]
-	puts $fd [format "#define DATE_SEC   (%d)" [lindex $dst 5]]
+	puts $fd [format "#define DATE_YEAR  (%d)" [forceInteger [lindex $dst 0]]]
+	puts $fd [format "#define DATE_MONTH (%d)" [forceInteger [lindex $dst 1]]]
+	puts $fd [format "#define DATE_DAY   (%d)" [forceInteger [lindex $dst 2]]]
+	puts $fd [format "#define DATE_HOUR  (%d)" [forceInteger [lindex $dst 3]]]
+	puts $fd [format "#define DATE_MIN   (%d)" [forceInteger [lindex $dst 4]]]
+	puts $fd [format "#define DATE_SEC   (%d)" [forceInteger [lindex $dst 5]]]
 	puts $fd [format ""]
 	puts $fd [format "#endif /* _config_h_ */"]
 	close $fd
